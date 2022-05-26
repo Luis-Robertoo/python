cd129b7139dbbd69091f948dce2b3b44de513e1a
luis <luis.r.s14@hotmail.com>
2022-05-25 21:12:54 -0300

    variaveis

---
+++ parte1/variaveis.py
@@ -0,0 +1,19 @@
+##variaveis
+##sem definir o tipo
+pais = "Brasil"
+num_titulos = 5
+preco = 7.42
+
+print(f"O {pais} já ganhou {num_titulos} titulos")
+print(f"Um valor de preco {preco}")
+print(f"tipo pais {type(pais)} \ntipo numTitulos {type(num_titulos)} \ntipo preço {type(preco)}")
+
+##definindo tipo ou forçando um tipo
+
+pais = str("Canada")
+num_titulos = int(0)
+preco = float(42.9)
+
+
+
+
