85fed66112f6488191780b476370d59166ea3b58
luis <luis.r.s14@hotmail.com>
2022-05-28 12:38:00 -0300

    formatando strings

---
+++ parte1/formatacao_string.py
@@ -0,0 +1,19 @@
+##https://docs.python.org/3/library/string.html#formatexamples
+
+print("R$ {:f}".format(1.58))
+
+print("R$ {:.2f}".format(1.58))
+
+print("R$ {:.2f}".format(1.5))
+
+print("R$ {:7.2f}".format(1.5))
+
+print("R$ {:7.2f}".format(1896.5))
+
+print("R$ {:07.2f}".format(1.5))
+
+print("R$ {:07d}".format(5))
+
+print("R$ {:7d}".format(5))
+
+print("data {:02d}/{:02d}/{:04d}".format(9,6,1998))
