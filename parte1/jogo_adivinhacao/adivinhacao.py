2d49840555e6d55f879cab9832a2d78eb9b6df4a
luis <luis.r.s14@hotmail.com>
2022-05-28 11:30:44 -0300

    fim dos elifs

--- parte1/jogo_adivinhacao/adivinhacao.py
+++ parte1/jogo_adivinhacao/adivinhacao.py
@@ -9,18 +9,18 @@ chute = int(input("Digite o seu número: "))
 
 print(f"Você digitou: {chute}")
 
-if(numero_secreto == chute):
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
     print("Você acertou!!")
 else:
-    if(chute > numero_secreto):
+    if(maior):
         print("Você errou, o chute foi maior que o número secreto!!")
-        
-    if(chute < numero_secreto):
+
+    elif(menor):
         print("Você errou, o chute foi menor que o número secreto!!")
-
-
-
-    print("Você errou!!")
 
 print("-*" * 17)
 print("Fim do jogo!!") 
