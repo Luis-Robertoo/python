a5c31790d2cda614f13bdcb52c077958627c0b94
luis <luis.r.s14@hotmail.com>
2022-05-28 11:48:37 -0300

    adicionado while e tentativas

--- parte1/jogo_adivinhacao/adivinhacao.py
+++ parte1/jogo_adivinhacao/adivinhacao.py
@@ -4,23 +4,30 @@ print("Bem vindo ao jogo da Adivinhação!")
 print("-*" * 17)
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
+while(total_de_tentativas >= rodada):
+    print(f"Rodada {rodada} de {total_de_tentativas}")
 
-print(f"Você digitou: {chute}")
+    chute = int(input("Digite o seu número: "))
+    print(f"Você digitou: {chute}")
+    
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    if(acertou):
+        print("Você acertou!!")
+        break
+    else:
+        if(maior):
+            print("Você errou, o chute foi maior que o número secreto!!")
 
-if(acertou):
-    print("Você acertou!!")
-else:
-    if(maior):
-        print("Você errou, o chute foi maior que o número secreto!!")
+        elif(menor):
+            print("Você errou, o chute foi menor que o número secreto!!")
 
-    elif(menor):
-        print("Você errou, o chute foi menor que o número secreto!!")
+    rodada += 1
 
 print("-*" * 17)
 print("Fim do jogo!!") 
