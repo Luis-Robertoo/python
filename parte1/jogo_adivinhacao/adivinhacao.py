7d15a0674932ccac2d6b5b4cf02dd170be7b9fab
luis <luis.r.s14@hotmail.com>
2022-05-28 11:19:11 -0300

    inicio elif

--- parte1/jogo_adivinhacao/adivinhacao.py
+++ parte1/jogo_adivinhacao/adivinhacao.py
@@ -1,5 +1,4 @@
 
-
 print("*-" * 17)
 print("Bem vindo ao jogo da Adivinhação!")
 print("-*" * 17)
@@ -13,6 +12,14 @@ print(f"Você digitou: {chute}")
 if(numero_secreto == chute):
     print("Você acertou!!")
 else:
+    if(chute > numero_secreto):
+        print("Você errou, o chute foi maior que o número secreto!!")
+        
+    if(chute < numero_secreto):
+        print("Você errou, o chute foi menor que o número secreto!!")
+
+
+
     print("Você errou!!")
 
 print("-*" * 17)
